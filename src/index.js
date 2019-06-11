9863cfc95d57fcd08dca2702a1d9d62a5070f33c
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2019-06-11 14:20:27 -0300

    Access post image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes'));
 
 app.listen(3030);
