b8e2f1971fd7e2e51f4f7c559f9d808345b47bea
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2019-06-11 14:28:23 -0300

    Real-time emit on post & like

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -30,6 +30,8 @@ module.exports = {
       image: fileName,
     });
 
+    req.io.emit('post', post);
+
     return res.json(post);
   },
 };
--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ const cors = require('cors');
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes'));
 
-app.listen(3030);
+server.listen(3030);
