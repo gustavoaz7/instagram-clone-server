577b584154d58d09ac6c9151d747d2011f486738
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2019-06-11 14:21:35 -0300

    Enable cors

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 
+app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes'));
 
